38c506b433f12db57c5d26dbb38a0d1e6b4c14ef
Sergio Oseko <osk.sergio@gmail.com>
2023-11-30 06:30:05 -0300

    test: aula #103 usando o find do capybara

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 feature "Welcomes", type: :feature do
   scenario 'Mostra a mensagem de boas-vindas' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Bem-Vindo')
+  end
+
+  scenario 'Verificar o link Cadastro de Clientes' do
+    visit(root_path)
+    #expect(page).to have_selector('ul li a')
+    #expect(page).to have_link('Cadastro de Clientes')
+    # em vez de usar as duas linhas acima, usar código abaixo (find >> capybara)
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
