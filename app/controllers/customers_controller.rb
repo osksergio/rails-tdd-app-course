2fb8a0da7f05a7dbb1574848b3a8fdcfdc414a96
Sergio Oseko <osk.sergio@gmail.com>
2023-12-09 08:46:37 -0300

    test: concluída aula #106 (cad. cliente parte 2)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,4 +6,18 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      redirect_to customers_path, notice: 'Cliente cadastrado com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  def customer_params
+    params.require(:customer).permit(:id, :name, :email, :smoker, :phone, :avatar)
+  end
 end
