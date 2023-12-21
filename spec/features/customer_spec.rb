be00b72d34f1ce12913836e3e03f8391ebbdaea1
Sergio Oseko <osk.sergio@gmail.com>
2023-12-21 06:24:12 -0300

    test: aula #113 (em andamento)

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -121,4 +121,17 @@ RSpec.feature "Customers", type: :feature do
     find(:xpath, "/html/body/table/tbody/tr[1]/td[3]/a").click
     expect(page).to have_content('Editando Cliente')
   end
+
+  scenario 'Excluir um cliente', js: true do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+
+  end
 end
